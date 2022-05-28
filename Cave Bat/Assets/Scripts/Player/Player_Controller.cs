d72871e2e01b703b09baf93e3f40b010c470728e
DangubaGitHub <dangubicjovan@outlook.com>
2022-05-28 18:00:10 +0200

    player movement ok for now

--- Cave Bat/Assets/Scripts/Player/Player_Controller.cs
+++ Cave Bat/Assets/Scripts/Player/Player_Controller.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    Rigidbody2D playerRb;
+
+    [SerializeField] float speed;
+    [SerializeField] float jump;
+    int playerDirection = -1;
+
+    private void Awake()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
     void Start()
     {
+        //playerRb.velocity = Vector2.left * speed;
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        playerRb.velocity = new Vector2(playerDirection * transform.localScale.x * speed, playerRb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            //speed = speed;
+            playerDirection = -1;
+        }
+        else if(Input.GetKeyDown(KeyCode.D))
+        {
+            //speed = -speed;
+            playerDirection = 1;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    void Jump()
+    {
+        //playerRb.velocity = new Vector2(jump * Time.deltaTime, playerRb.velocity.x);
+        playerRb.velocity = Vector2.up * jump;
     }
 }
